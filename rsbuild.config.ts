7bbe80cb9f56b3fe5c5be29ca9b2312211a0a3b5
Spencer Alves <impiaaa@gmail.com>
2026-05-22 16:28:59 -0700

    Add software dependencies to in-app credits

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -5,6 +5,7 @@ import { pluginTypeCheck } from "@rsbuild/plugin-type-check";
 import { pluginHtmlMinifierTerser } from "rsbuild-plugin-html-minifier-terser";
 import { pluginI18nextExtractor } from "rsbuild-plugin-i18next-extractor";
 import { pluginWasmPack } from "rsbuild-plugin-wasmpack";
+import { parse as parseToml } from "smol-toml";
 import i18nextToolkitConfig from "./i18next.config.ts";
 
 function genSoftwareLicenses() {
@@ -12,19 +13,55 @@ function genSoftwareLicenses() {
   const packages = JSON.parse(
     readFileSync("package-lock.json", "utf-8"),
   ).packages;
-  for (const package_name in packages) {
-    if (!package_name) continue;
-    const pkg = packages[package_name];
-    if (
-      pkg.dev ||
-      pkg.devOptional ||
-      !pkg.license ||
-      !package_name.startsWith("node_modules/")
-    )
-      continue;
-    const short_name = package_name.substring("node_modules/".length);
-    s += `<li>${short_name} ${pkg.version} is licensed under ${pkg.license}.</li>`;
-  }
+  const direct_dependencies: Record<string, string> = packages[""].dependencies;
+  const licenses = new Set(
+    Object.entries(packages)
+      .filter(
+        ([package_name, _]) =>
+          !!direct_dependencies[
+            package_name.startsWith("node_modules/")
+              ? package_name.substring("node_modules/".length)
+              : package_name
+          ],
+      )
+      .map(([_, pkg]: [string, any]) => pkg.license),
+  );
+  licenses.forEach((license) => {
+    if (!license) return;
+    s += "<li>";
+    let count = 0;
+    for (const package_name in packages) {
+      if (!package_name) continue;
+      const short_name = package_name.startsWith("node_modules/")
+        ? package_name.substring("node_modules/".length)
+        : package_name;
+      if (!direct_dependencies[short_name]) continue;
+      const pkg = packages[package_name];
+      if (pkg.dev || pkg.devOptional || pkg.license !== license) continue;
+      if (count > 0) s += ", ";
+      s += `<a href="https://www.npmjs.com/package/${short_name}/v/${pkg.version}" target="_blank">${short_name} ${pkg.version}</a>`;
+      count += 1;
+    }
+    s += ` ${count === 1 ? "is" : "are"} licensed under ${license}</li>`;
+  });
+
+  s += "<li>";
+  // const rust_packages = parseToml(readFileSync("gen/Cargo.lock", "utf-8")).package;
+  // (rust_packages.valueOf() as any[]).forEach((pkg, i) => {
+  //   if (i > 0) s += ", ";
+  //   s += `<a href="https://crates.io/crates/${pkg.name}/${pkg.version}" target="_blank">${pkg.name} ${pkg.version}</a>`;
+  // });
+  const rust_packages = parseToml(
+    readFileSync("gen/Cargo.toml", "utf-8"),
+  ).dependencies;
+  Object.entries(rust_packages.valueOf() as object).forEach(
+    ([package_name, pkg], i) => {
+      if (i > 0) s += ", ";
+      s += `<a href="https://crates.io/crates/${package_name}/${pkg.version}" target="_blank">${package_name} ${pkg.version}</a>`;
+    },
+  );
+  s += "</li>";
+
   return s;
 }
 
